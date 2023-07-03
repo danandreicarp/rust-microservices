b9b4be8bbddd8d48c181082024c700d9c1bc0eef
carpdan <carpdan@proton.me>
2023-07-03 21:52:59 +0300

    add regex parsing to server paths

--- 02/hyper-microservice-new/src/main.rs
+++ 02/hyper-microservice-new/src/main.rs
@@ -9,6 +9,8 @@ use hyper::{
     service::{make_service_fn, service_fn},
     Body, Method, Request, Response, Server, StatusCode,
 };
+use lazy_static::lazy_static;
+use regex::Regex;
 use slab::Slab;
 
 const INDEX: &'static str = r#"
@@ -23,7 +25,11 @@ const INDEX: &'static str = r#"
  </html>
  "#;
 
-const USER_PATH: &str = "/user/";
+lazy_static! {
+    static ref INDEX_PATH: Regex = Regex::new("^/(index\\.html?)?$").unwrap();
+    static ref USER_PATH: Regex = Regex::new("^/user/((?P<user_id>\\d+?)/?)?$").unwrap();
+    static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap();
+}
 
 type UserId = u64;
 struct UserData;
@@ -71,52 +77,68 @@ async fn microservice_handler(
     user_db: UserDb,
 ) -> Result<Response<Body>, Infallible> {
     let response = {
-        match (req.method(), req.uri().path()) {
-            (&Method::GET, "/") => Response::new(INDEX.into()),
-            (method, path) if path.starts_with(USER_PATH) => {
-                let user_id = path
-                    .trim_start_matches(USER_PATH)
-                    .parse::<UserId>()
-                    .ok()
-                    .map(|x| x as usize);
+        let method = req.method();
+        let path = req.uri().path();
+        let mut users = user_db.lock().unwrap();
 
-                let mut users = user_db.lock().unwrap();
-
-                match (method, user_id) {
-                    (&Method::POST, None) => {
-                        let id = users.insert(UserData);
-                        Response::new(id.to_string().into())
-                    }
-                    (&Method::POST, Some(_)) => response_with_code(StatusCode::BAD_REQUEST),
-                    (&Method::GET, Some(id)) => {
-                        if let Some(data) = users.get(id) {
-                            Response::new(data.to_string().into())
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    }
-                    (&Method::PUT, Some(id)) => {
-                        if let Some(user) = users.get_mut(id) {
-                            *user = UserData;
-                            response_with_code(StatusCode::OK)
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    }
-                    (&Method::DELETE, Some(id)) => {
-                        if users.contains(id) {
-                            users.remove(id);
-                            response_with_code(StatusCode::OK)
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    }
-                    _ => response_with_code(StatusCode::METHOD_NOT_ALLOWED),
-                }
+        if INDEX_PATH.is_match(path) {
+            if method == &Method::GET {
+                Response::new(INDEX.into())
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
             }
-            _ => response_with_code(StatusCode::NOT_FOUND),
+        } else if USERS_PATH.is_match(path) {
+            if method == &Method::GET {
+                let list = users
+                    .iter()
+                    .map(|(id, _)| id.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",");
+                Response::new(list.into())
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+            }
+        } else if let Some(cap) = USER_PATH.captures(path) {
+            let user_id = cap
+                .name("user_id")
+                .and_then(|m| m.as_str().parse::<UserId>().ok().map(|x| x as usize));
+
+            match (method, user_id) {
+                (&Method::POST, None) => {
+                    let id = users.insert(UserData);
+                    Response::new(id.to_string().into())
+                }
+                (&Method::POST, Some(_)) => response_with_code(StatusCode::BAD_REQUEST),
+                (&Method::GET, Some(id)) => {
+                    if let Some(data) = users.get(id) {
+                        Response::new(data.to_string().into())
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                }
+                (&Method::PUT, Some(id)) => {
+                    if let Some(user) = users.get_mut(id) {
+                        *user = UserData;
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                }
+                (&Method::DELETE, Some(id)) => {
+                    if users.contains(id) {
+                        users.remove(id);
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                }
+                _ => response_with_code(StatusCode::METHOD_NOT_ALLOWED),
+            }
+        } else {
+            response_with_code(StatusCode::NOT_FOUND)
         }
     };
+
     Ok(response)
 }
 
